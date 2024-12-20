f0eae5670ff30947223c9008bb2f295128321247
mmco49 <mmco.910516@gmail.com>
2024-12-20 09:21:41 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,86 @@
+/*=================================================
+  ハンバーガ―メニュー
+  ===================================================*/
+  // ハンバーガーメニューをクリックした時
+  $(".toggle_btn").on("click", function () {
+    // toggleClassを使用することで、hamburgerクラスにactiveクラスが存在する場合は削除、
+    // 存在しない場合を追加する処理を自動で行ってくれる
+    $("header").toggleClass("open");
+  });
+  // メニューのリンクをクリックした時
+  // $("#navi a").on("click", function () {
+  //   $("header").toggleClass("open");
+  // });
+
+/*=================================================
+  スクロール時の画像フェード表示
+  ===================================================*/
+  $(window).scroll(function () {
+    // fadeinクラスに対して順に処理を行う
+    $(".fadein").each(function () {
+      // スクロールした距離
+      let scroll = $(window).scrollTop();
+      // fadeinクラスの要素までの距離
+      let target = $(this).offset().top;
+      // 画面の高さ
+      let windowHeight = $(window).height();
+      // fadeinクラスの要素が画面下にきてから200px通過した
+      // したタイミングで要素を表示
+      if (scroll > target - windowHeight + 120) {
+        $(this).css("opacity", "1");
+        $(this).css("transform", "translateY(0)");
+      }
+    });
+  });
+
+  /*=================================================
+  トップに戻る
+  ===================================================*/
+  let pagetop = $(".menu-btn");
+  // 最初に画面が表示された時は、トップに戻るボタンを非表示に設定
+  pagetop.hide();
+
+  // スクロールイベント（スクロールされた際に実行）
+  $(window).scroll(function () {
+    // スクロール位置が700pxを超えた場合
+    if ($(this).scrollTop() > 850) {
+      // トップに戻るボタンを表示する
+      pagetop.fadeIn();
+
+      // スクロール位置が700px未満の場合
+    } else {
+      // トップに戻るボタンを非表示にする
+      pagetop.fadeOut();
+    }
+  });
+
+  /*=================================================
+  スムーススクロール
+  ===================================================*/
+  // ページ内リンクのイベント
+  $('a[href^="#"]').click(function () {
+    // aタグのhref属性の値が#で始まる要素をクリックした時に実行する
+    // 'a[href^=#]'：「aタグのhref属性で値が#で始まる要素だったとき」
+
+    // リンクを取得 クリックされたaタグのhref属性の中身をhrefという変数に代入する （#menuなど）をhrefという変数に代入する
+    let href = $(this).attr("href");
+    // this: クリックされたaタグ $('a[href^=#]')
+    // .attr()は、要素の属性の値を取得する
+
+    // ジャンプ先のid名をセット href == "#" 】 変数hrefの値が"#"【 || 】 または【href == ""】  であれば（【？】）
+    // 【 $('html'); 】 へのリンク（≒ページトップ）,そうでなければ（【:】）【 $(href); 】 変数hrefの中身が到着地点になる
+    let target = $(href == "#" || href == "" ? "html" : href);
+
+    // トップからジャンプ先の要素までの距離を取得 （id=menuなどがページの一番上から何pxかを取得）
+    let position = target.offset().top;
+    // offset()は表示位置を取得する offset().topでページの一番上から何pxかを取得
+
+    // animateでスムーススクロールを行う ページトップからpositionだけスクロールする
+    // 600はスクロール速度で単位はミリ秒 swingはイージングのひとつ
+    $("html, body").animate({ scrollTop: position }, 600, "swing");
+    return false;
+  });
+  // {scrollTop:position}で、ページトップからposition分だけスクロールするという指定をしているいる。
+  // linear：常に同じ速さで動く swing：始めはゆっくり動いて、途中はちょっと速め、最後はゆっくりと動く
+  // 出発地点をクリックすると、URLの末尾にIDタグ(例.https://coffee.com#menu)が付与されてしまう。
+  // これを防ぐために、最後に１文return falseを追加します。
